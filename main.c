3db18254a78e3d294ffad9700f43b2a7d09829e4
Rohit Bhoge <133008672+GaLv0331@users.noreply.github.com>
2025-11-30 20:30:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,41 @@
 #include "tic_tac_toe.h" //user defined function
 
 int main(){
+	
+	char *player1 = NULL, *player2 = NULL;
+	playerInfo(&player1, &player2);
+	
 	char *board[COLUMN];
+
 	for(int i=0; i<ROW; i++){
 		board[i] = (char *)calloc(COLUMN, sizeof(char));
 		if(board[i]==NULL){
 			perror("Memory Allocation Failed");
-			exit(-1);
+			free(player1);
+			free(player2);
+			exit(EXIT_FAILURE);
 		}
 	} //dynamically allocating size to 2d array with array of pointers
 	
 	initialize_board(board); //initializing board with 0 value
 	
-	int turn=0;// to count turn which player
-	
+	int turn=0, gameState = GAME_PENDING;// to count turn which player
 	printf("\n\nFor row and column enter between 1-3");
 	
-	while(1){
-	
-		if(turn>8){
-			printf("\nTie Occured");
-			free_mem(board);
-			exit(0);
-		} //to check if all blocks are played or not
-		
-		printf("\n=================Board=================\n");
-		for(int i=0; i<ROW; i++){
-			for(int j=0; j<COLUMN; j++){
-				printf(" %c", board[i][j]);
-			}
-			printf("\n");
-		} //to display the board after each turn
+	while(gameState != GAME_OVER){
 			
 		if(turn%2==0){
-			if(turn_player1(board, &turn)==0){
-				free_mem(board);
-				exit(0);
-			}; //if player 1 won free the allocated memory
+			gameState = playerTurn(board, turn, player1, 'X');
 		} //player 1 turn
 		
 		if(turn%2==1){
-			if(turn_player2(board, &turn)==0){
-				free_mem(board);
-				exit(0);
-			}; //if player 2 won free the allocated memory
+			gameState = playerTurn(board, turn, player2, 'O');
 		} //player 2 turn
-		
-		turn++; 
+		if(gameState != GAME_OVER){
+			turn++;
+		}
+		displayBoard(board);			
 	}
+	
+	free_mem(board, player1, player2);
 }
