6caea1c506da69ff5b6b2ca29ff83abd2f8e812f
Rohit Bhoge <133008672+GaLv0331@users.noreply.github.com>
2025-11-30 20:30:49 +0530

    Update tic_tac_toe.h

--- tic_tac_toe.h
+++ tic_tac_toe.h
@@ -3,19 +3,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //===================
 //defined macros for board size
-#define ROW 3
-#define COLUMN 3
+#define ROW 		3
+#define COLUMN 	3
+
+#define WON		0
+#define TIE		1
+#define PENDING	2
+
+#define PLACED		0
+#define OCCUPIED	1
+#define INVALID_POS	2
+
+#define GAME_OVER	0
+#define GAME_PENDING	1
 //===================
 //function declarations
-int pos_occupied_player1(char *board[COLUMN], int *row, int *column);
-int pos_occupied_player2(char *board[COLUMN], int *row, int *column);
-int win_conditions_player1(char *board[COLUMN]);
-int win_conditions_player2(char *board[COLUMN]);
+void playerInfo(char **player1, char**player2);
+void displayBoard(char *board[COLUMN]);
 void initialize_board(char *board[COLUMN]);
-int  turn_player1(char *board[COLUMN], int *turn);
-int turn_player2(char *board[COLUMN], int *turn);
-void free_mem(char *boar[COLUMN]);
+int winCondition(char *board[COLUMN], char playerMark, int turn);
+int playerTurn(char *board[COLUMN], int turn, char *name, char playerMark);
+int putMark(char *board[COLUMN], int row, int col, char mark);
+void free_mem(char *boar[COLUMN], char *player1, char *player2);
 
 #endif
